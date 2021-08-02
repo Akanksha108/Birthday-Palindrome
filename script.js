df8876fa34daff63ba0a1d0f716420340473afc9
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-08-02 17:13:35 +0530

    added different date formats

--- script.js
+++ script.js
@@ -8,6 +8,45 @@ let reverseNumber = 0;
 
 let output = document.getElementById("output");
 
+
+function combinations(year, month, date){
+
+    // yyyy-mm-dd
+    const format1 = year + month + date;
+
+    // dd-mm-yyyy
+    const format2 = date + month + year;
+
+    // mm-dd-yy
+    const format3 = month + date + year.substring(2,4);
+
+    // m-dd-yyyy
+    const format4 = Number(month) + date + year;
+}
+
+
+function isPalindrome(format){
+
+    let formatCopy = format;
+
+    for(let i = 0; i < format.length; i++){
+
+        let digit = formatCopy % 10;
+
+        reverseNumber = (reverseNumber * 10) + digit;
+
+        formatCopy = Math.floor(formatCopy / 10);
+    }
+    console.log(reverseNumber);
+
+    if(format == reverseNumber){
+        output.innerHTML = "Hurray it is a palindrome!!!!";
+    }
+    else{
+        output.innerHTML ="It is not a palindrome";
+    }
+}
+
 submitBtn.addEventListener("click", function(event){
 
     event.preventDefault();
@@ -18,39 +57,16 @@ submitBtn.addEventListener("click", function(event){
 
     let year = dateArr[0];
     let month = dateArr[1];
-    let day = dateArr[2];
+    let date = dateArr[2];
+
+    let flag = combinations(year, month, date);
 
 
-    // Format : 1995-08-01
-    for(let i = 0; i < bdate.length; i++){
+   
+    console.log(format1);
 
-         let num = parseInt(bdate.charAt(i));
+    isPalindrome(format1);
 
-         if(!isNaN(num)){
-             let currentDigit = num % 10;
-
-             dateConvToNumber = (dateConvToNumber * 10) + currentDigit; 
-         }
-    }
-
-    let dateConvToNumberCopy1 = dateConvToNumber;
-
-    for(let i = 0; i < bdate.length - 2; i++){
-
-        let digit = dateConvToNumberCopy1 % 10;
-
-        reverseNumber = (reverseNumber * 10) + digit;
-
-        dateConvToNumberCopy1 = Math.floor(dateConvToNumberCopy1 / 10);
-    }
-
-
-    if(dateConvToNumber == reverseNumber){
-        output.innerHTML = "Hurray it is a palindrome!!!!";
-    }
-    else{
-        output.innerHTML ="It is not a palindrome";
-    }
 
 });
 
